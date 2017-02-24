112994d3a7f8a8c232f4bbdb2bf3497c636676a5
Cody Mercer <me@codymercer.com>
2017-02-23 20:50:14 -0600

    simplify app.vue

--- components/app.vue
+++ components/app.vue
@@ -1,14 +1,6 @@
 <template>
 <div id="app">
   <router-view></router-view>
-  <ul>
-    <li>
-      <router-link to="/">Home</router-link>
-    </li>
-    <li>
-      <router-link to="/test">Test</router-link>
-    </li>
-  </ul>
 </div>
 </template>
 
